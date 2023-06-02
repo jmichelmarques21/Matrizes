572cb503c5b48e2f535f95c26491bd0a8819cb4b
Jean Marques <jmichelmarques21@gmail.com>
2023-06-02 00:16:12 -0300

    Adicionar arquivos de projeto.

---
+++ Matrizes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Matrizes {
+    class Program {
+        static void Main(string[] args) {
+
+            int cont = 0;
+
+            Console.Write("Qual é a ordem da matriz? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+
+            for (int i = 0; i < n; i++) {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++) {
+                    mat[i, j] = int.Parse(values[j]);
+                    if (mat[i, j] < 0) {
+                        cont++;
+                    }
+                }
+            }
+
+            Console.Write("Main diagonal: ");
+            for (int i = 0; i < n; i++) {
+                Console.Write(mat[i,i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Negative numbers: " + cont);
+
+
+
+
+        }
+    }
+}
